84a67b02cd49a7b7feb4ea124b4ffa48873339b7
Grigoriy Arzhevitin <arzhevitin.g@gmail.com>
2025-09-10 15:32:14 +0500

    bump zapret-bolvan version

--- zapret-bolvan/zapret-winws/preset_discord_media_stun.cmd
+++ zapret-bolvan/zapret-winws/preset_discord_media_stun.cmd
@@ -1,3 +1,3 @@
 start "zapret: discord_media,stun" /min "%~dp0winws.exe" ^
 --wf-raw=@"%~dp0windivert.filter\windivert.discord_media+stun.txt" ^
---filter-l7=discord,stun --dpi-desync=fake
+--filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=2
--- zapret-bolvan/zapret-winws/preset_wireguard.cmd
+++ zapret-bolvan/zapret-winws/preset_wireguard.cmd
@@ -1,3 +1,3 @@
 start "zapret: wireguard" /min "%~dp0winws.exe" ^
 --wf-raw=@"%~dp0windivert.filter\windivert.wireguard.txt" ^
---filter-l7=wireguard --dpi-desync=fake
+--filter-l7=wireguard --dpi-desync=fake --dpi-desync-repeats=2
